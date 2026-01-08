e390baa095ffd40778eb01903db6002b34e45d72
Michael Sanchezmsanch74@gmail.com <you@example.com>
2026-01-08 17:34:39 +0000

    revised security settings

--- data.tf
+++ data.tf
@@ -38,7 +38,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
     security_group_id = aws_security_group.allow_tls.id
     cidr_ipv4 = "0.0.0.0/0"
-    from_port = 22
+    from_port = 80
     ip_protocol = "tcp"
-    to_port = 22
+    to_port = 80
 }
