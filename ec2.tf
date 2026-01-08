56e7f96f2c219a1ef466c9de1aec908419be7cfe
Michael Sanchezmsanch74@gmail.com <you@example.com>
2026-01-08 17:59:04 +0000

    new security updates to ec2 file

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "example" {
   ami           = var.allen-ami
   instance_type = var.vm-size
 
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
   tags = {
     Name = var.vm-name
   }
